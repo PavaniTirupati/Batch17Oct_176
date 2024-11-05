448d43093d58f93989bd9251316ea011371e9d88
PavaniTirupati <tpavani3012@gmail.com>
2024-11-05 21:45:00 +0000

    If else code 1 updated

---
+++ Assignments/iesct.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+ int main()
+ {
+         int num1,num2,num3,res;
+         printf("Enter num1: ");
+         scanf("%d",&num1);
+         printf("\nEnter num2: ");
+         scanf("%d",&num2);
+         printf("\nEnter num3: ");
+         scanf("%d",&num3);
+         res=(num1<num2)?((num1<num3)?num1:num3):((num2<num3)?num2:num3);
+         printf("\n%d is the smallest\n",res);
+         return 0;
+}
+

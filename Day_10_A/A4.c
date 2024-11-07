55f862b499590868ae958cbb5bc8f70147c01c35
PavaniTirupati <163111747+PavaniTirupati@users.noreply.github.com>
2024-11-07 21:32:34 +0530

    Update A4.c

--- Day_10_A/A4.c
+++ Day_10_A/A4.c
@@ -1,23 +1,28 @@
-/* DESCRIPTION : program to rotate array every kth element
+/* 
+File : A4.c
+DESCRIPTION : program to rotate array every kth element
 Date : 7-11-2024
 Name : Tirupati Pavani
 */
 
 #include <stdio.h>
 
-void reverse(int arr[], int start, int end) {
-    while (start<end) {
+void reverse(int arr[], int start, int end) 
+{
+    while (start<end) 
+    {
         int temp = arr[start];
         arr[start] = arr[end];
         arr[end] = temp;
-        
         start++;
         end--;
     }
 }
 
-void rotateArrayEveryKthElement(int arr[], int size, int k) {
-    for (int i=0;i<size;i+=k) {
+void rotateArrayEveryKthElement(int arr[], int size, int k) 
+{
+    for (int i=0;i<size;i+=k) 
+    {
         int end = (i+k-1 < size) ? (i+k-1) : (size-1);
         reverse(arr, i, end);
     }
@@ -25,26 +30,21 @@ void rotateArrayEveryKthElement(int arr[], int size, int k) {
 
 int main() {
     int size, k;
-    
     printf("Enter the number of elements in the array: ");
     scanf("%d", &size);
-    
     int arr[size];
-    
     printf("Enter the elements of the array: ");
-    for (int i=0;i<size;i++) {
+    for (int i=0;i<size;i++) 
+    {
         scanf("%d", &arr[i]);
     }
-    
     printf("Enter the value of k: ");
     scanf("%d", &k);
-    
     printf("\n");
-    
     rotateArrayEveryKthElement(arr, size, k);
-    
     printf("Array after rotating every %d-th element: ", k);
-    for (int i=0;i<size;i++) {
+    for (int i=0;i<size;i++) 
+    {
         printf("%d ", arr[i]);
     }
     printf("\n");

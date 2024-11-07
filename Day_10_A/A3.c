38fd0e606f844d308b45a73ef7595b34ea1c29e3
PavaniTirupati <163111747+PavaniTirupati@users.noreply.github.com>
2024-11-07 22:04:06 +0530

    Add files via upload

---
+++ Day_10_A/A3.c
@@ -0,0 +1,41 @@
+/* 
+File : A3.c
+DESCRIPTION : program to find the  n-th number made of prime digits
+Date : 7-11-2024
+Name : Tirupati Pavani
+*/
+
+#include <stdio.h>
+
+void findNthNumber(int N);
+int main() {
+    int T, N;
+    printf("Enter the number of test cases: ");
+    scanf("%d", &T);
+    for (int i=0;i<T;i++) {
+        printf("Enter the value of N: ");
+        scanf("%d", &N);
+        findNthNumber(N);
+    }
+
+    return 0;
+}
+
+void findNthNumber(int N) 
+{
+    int primeDigits[] = {2, 3, 5, 7};
+    int result[10]; 
+    int index = 0;
+    while (N > 0) 
+    {
+        int remainder = (N - 1) % 4;  
+        result[index++] = primeDigits[remainder]; 
+        N = (N - 1) / 4;  
+    }
+
+    printf("The %d-th number made of prime digits is: ", N);
+    for (int i = index - 1; i >= 0; i--) {
+        printf("%d", result[i]);  
+    }
+    printf("\n");
+}

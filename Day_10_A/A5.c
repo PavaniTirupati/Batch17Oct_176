7a47acb2c8599ed967137818077529066ee6e509
PavaniTirupati <163111747+PavaniTirupati@users.noreply.github.com>
2024-11-07 21:20:03 +0530

    Add files via upload

---
+++ Day_10_A/A5.c
@@ -0,0 +1,51 @@
+/* DESCRIPTION : To print the triangle pattern
+Date : 7-11-2024
+Name : Tirupati Pavani
+File : A5.c
+*/
+
+#include <stdio.h>
+
+void alphabetTriangle(int n){
+    for(int i=1;i<=n;i++){
+        printf("%*c", n - i + 1, ' ');
+        for (int j = 1; j <= i; j++) {
+            printf("%c", 'A' + j - 1);
+        }
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%c", 'A' + j - 1);
+        }
+
+        printf("\n");
+    }
+}
+
+void numTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+        printf("%*c", n - i + 1, ' '); 
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+
+        printf("\n");  
+    }
+}
+
+
+int main(){
+    int n;
+    printf("Enter the number of rows for the triangles: ");
+    scanf("%d", &n);
+    printf("Alphabet Triangle\n");
+    alphabetTriangle(n);
+    
+    printf("\n");
+    
+    printf("Number Triangle\n");
+    numTriangle(n);
+    
+    return 0;
+}

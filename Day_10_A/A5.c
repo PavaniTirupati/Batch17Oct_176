de285f1ba0d96a1df9162496b1d053b87c229ceb
PavaniTirupati <163111747+PavaniTirupati@users.noreply.github.com>
2024-11-07 21:35:36 +0530

    Update A5.c

--- Day_10_A/A5.c
+++ Day_10_A/A5.c
@@ -1,18 +1,39 @@
-/* DESCRIPTION : To print the triangle pattern
+/* 
+File : A5.c
+DESCRIPTION : To print the triangle pattern
 Date : 7-11-2024
 Name : Tirupati Pavani
-File : A5.c
 */
 
 #include <stdio.h>
 
-void alphabetTriangle(int n){
-    for(int i=1;i<=n;i++){
+void numTriangle(int n);
+void alphabetTriangle(int n);
+int main()
+{
+    int n;
+    printf("Enter the number of rows for the triangles: ");
+    scanf("%d", &n);
+    printf("Alphabet Triangle\n");
+    alphabetTriangle(n);
+    printf("\n");
+    printf("Number Triangle\n");
+    numTriangle(n);
+    
+    return 0;
+}
+
+void alphabetTriangle(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
         printf("%*c", n - i + 1, ' ');
-        for (int j = 1; j <= i; j++) {
+        for (int j = 1; j <= i; j++) 
+        {
             printf("%c", 'A' + j - 1);
         }
-        for (int j = i - 1; j >= 1; j--) {
+        for (int j = i - 1; j >= 1; j--) 
+        {
             printf("%c", 'A' + j - 1);
         }
 
@@ -20,13 +41,17 @@ void alphabetTriangle(int n){
     }
 }
 
-void numTriangle(int n) {
-    for (int i = 1; i <= n; i++) {
+void numTriangle(int n) 
+{
+    for (int i = 1; i <= n; i++) 
+    {
         printf("%*c", n - i + 1, ' '); 
-        for (int j = 1; j <= i; j++) {
+        for (int j = 1; j <= i; j++) 
+        {
             printf("%d", j);
         }
-        for (int j = i - 1; j >= 1; j--) {
+        for (int j = i - 1; j >= 1; j--) 
+        {
             printf("%d", j);
         }
 
@@ -34,18 +59,3 @@ void numTriangle(int n) {
     }
 }
 
-
-int main(){
-    int n;
-    printf("Enter the number of rows for the triangles: ");
-    scanf("%d", &n);
-    printf("Alphabet Triangle\n");
-    alphabetTriangle(n);
-    
-    printf("\n");
-    
-    printf("Number Triangle\n");
-    numTriangle(n);
-    
-    return 0;
-}
